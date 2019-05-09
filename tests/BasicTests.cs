46eac9e0687f5e26739195b14ff81fa2e42e599c
seba gomez <sebagomezcorrea@outlook.com>
2019-05-08 22:33:46 -0300

    fixe tests with Azure Key Vault keys

--- tests/BasicTests.cs
+++ tests/BasicTests.cs
@@ -1,9 +1,13 @@
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Sebagomez.BitLyHelper;
 using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -19,52 +23,17 @@ namespace BitLyTests
 			this.output = output;
 		}
 
-		public static string APILogin { get; set; }
-		public static string APIKey { get; set; }
-
-		const string API_LOGIN = "BITLY_API_LOGIN";
-		const string API_KEY = "BITLY_API_KEY";
-
-		static BasicTests()
-		{
-#if DEBUG
-			if (File.Exists("Properties\\launchSettings.json"))
-			{
-				using (var file = File.OpenText("Properties\\launchSettings.json"))
-				{
-					var reader = new JsonTextReader(file);
-					var jObject = JObject.Load(reader);
-
-					var variables = jObject
-						.GetValue("profiles")
-						.SelectMany(profiles => profiles.Children())
-						.SelectMany(profile => profile.Children<JProperty>())
-						.Where(prop => prop.Name == "environmentVariables")
-						.SelectMany(prop => prop.Value.Children<JProperty>())
-						.ToList();
-
-					foreach (var variable in variables)
-					{
-						Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
-					}
-				}
-			}
-#endif
-			APILogin = Environment.GetEnvironmentVariable(API_LOGIN);
-			APIKey = Environment.GetEnvironmentVariable(API_KEY);
-
-			if (string.IsNullOrEmpty(APIKey) || string.IsNullOrEmpty(APILogin))
-				throw new Exception("APIKey and/or APILogin not defined");
-		}
-
 
 		const string GOOGLE = "http://google.com";
 		const string SHORT_GOOGLE = "http://bit.ly/2L6Cz94";
 
+
+		
+
 		[Fact]
 		public async Task JustUrl()
 		{
-			BitLyShortener shortener = new BitLyShortener(APILogin, APIKey);
+			BitLyShortener shortener = new BitLyShortener(Credentials.Current.APILOGIN, Credentials.Current.APIKEY);
 			string shortened = await shortener.ShortenUrl(GOOGLE);
 
 			Assert.Equal(SHORT_GOOGLE, shortened);
@@ -74,7 +43,7 @@ namespace BitLyTests
 		public async Task ShortText()
 		{
 			string template = "This is a string with Google url {0}";
-			BitLyShortener shortener = new BitLyShortener(APILogin, APIKey);
+			BitLyShortener shortener = new BitLyShortener(Credentials.Current.APILOGIN, Credentials.Current.APIKEY);
 			string shortened = await shortener.GetShortenString(string.Format(template, GOOGLE));
 
 			Assert.Equal(string.Format(template, SHORT_GOOGLE), shortened);
@@ -84,7 +53,7 @@ namespace BitLyTests
 		public async Task NoUrl()
 		{
 			string text = "This text has no urls";
-			BitLyShortener shortener = new BitLyShortener(APILogin, APIKey);
+			BitLyShortener shortener = new BitLyShortener(Credentials.Current.APILOGIN, Credentials.Current.APIKEY);
 			string shortened = await shortener.GetShortenString(text);
 
 			Assert.Equal(text, shortened);
---
+++ tests/Credentials.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BitLyTests
+{
+	public sealed class Credentials
+	{
+		//KeyVault stuff taken from https://stackoverflow.com/questions/47875589/cant-access-azure-key-vault-from-desktop-console-app
+		public static Credentials Current = new Credentials();
+
+		const string API_LOGIN = "BITLY-API-LOGIN";
+		const string API_KEY = "BITLY-API-KEY";
+		const string VAULT_URL = "VAULT_URL";
+		const string APPLICATION_ID = "APPLICATION_ID";
+		const string APPLICATION_SECRET = "APPLICATION_SECRET";
+
+#if DEBUG
+
+		private Credentials()
+		{
+			if (File.Exists("Properties\\launchSettings.json"))
+			{
+				using (var file = File.OpenText("Properties\\launchSettings.json"))
+				{
+					var reader = new JsonTextReader(file);
+					var jObject = JObject.Load(reader);
+
+					var variables = jObject
+						.GetValue("profiles")
+						.SelectMany(profiles => profiles.Children())
+						.SelectMany(profile => profile.Children<JProperty>())
+						.Where(prop => prop.Name == "environmentVariables")
+						.SelectMany(prop => prop.Value.Children<JProperty>())
+						.ToList();
+
+					foreach (var variable in variables)
+					{
+						Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+					}
+				}
+			}
+		}
+#endif
+
+		#region Properties
+
+		private string m_apiLogin;
+		public string APILOGIN
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(m_apiLogin) || string.IsNullOrEmpty(m_apiKey))
+					LoadCredentials();
+
+				return m_apiLogin;
+			}
+		}
+
+		private string m_apiKey;
+		public string APIKEY
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(m_apiLogin) || string.IsNullOrEmpty(m_apiKey))
+					LoadCredentials();
+
+				return m_apiKey;
+			}
+		}
+
+		private string m_vaultUrl;
+		public string VaultUrl
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(m_vaultUrl))
+					m_vaultUrl = Environment.GetEnvironmentVariable(VAULT_URL);
+
+				return m_vaultUrl;
+			}
+		}
+
+		private string m_appId;
+		public string ApplicationId
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(m_appId))
+					m_appId = Environment.GetEnvironmentVariable(APPLICATION_ID);
+
+				return m_appId;
+			}
+		}
+
+		private string m_appSecret;
+		public string ApplicationSecret
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(m_appSecret))
+					m_appSecret = Environment.GetEnvironmentVariable(APPLICATION_SECRET);
+
+				return m_appSecret;
+			}
+		}
+
+		#endregion
+
+		void LoadCredentials()
+		{
+			m_apiLogin = GetSecretAsync(VaultUrl, API_LOGIN).Result;
+			m_apiKey = GetSecretAsync(VaultUrl, API_KEY).Result;
+		}
+
+		private async Task<string> GetSecretAsync(string vaultUrl, string vaultKey)
+		{
+			var client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessTokenAsync), new HttpClient());
+			var secret = await client.GetSecretAsync(vaultUrl, vaultKey);
+
+			return secret.Value;
+		}
+
+		private async Task<string> GetAccessTokenAsync(string authority, string resource, string scope)
+		{
+			var appCredentials = new ClientCredential(ApplicationId, ApplicationSecret);
+			var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+
+			var result = await context.AcquireTokenAsync(resource, appCredentials);
+
+			return result.AccessToken;
+		}
+
+		
+	}
+}
